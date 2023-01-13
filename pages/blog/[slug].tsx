62a655719df58622bda4ac62c975028d859bc401
Bedrock02 <jimsteve91@gmail.com>
2023-01-13 13:36:45 -0500

    Temporary solution for styles in pages

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,9 +1,52 @@
 import fs from "fs";
 import matter from "gray-matter";
 import ReactMarkdown from 'react-markdown';
-import blogStyles from '../../styles/pages/blog.styles';
 import { readTime } from "../../lib/utils";
 import { AiFillCalendar, AiFillClockCircle } from 'react-icons/ai'
+
+import { generateTailWindStrings } from "../../lib/utils"
+
+const blogStyles = generateTailWindStrings({
+    articleContainer: [
+        'bg-stone-100',
+        'prose',
+        'max-w-full',
+        'mx-auto',
+        'px-6',
+    ],
+    header: [
+        'align-middle',
+        'flex',
+        'flex-col',
+        'items-center',
+        'justify-evenly',
+        'py-10',
+    ],
+    metaDesc: [
+        'w-3/5',
+        'text-center',
+        'text-base',
+        'text-stone-500',
+    ],
+    metaData: [
+        'flex',
+        'justify-around',
+        'text-stone-500',
+        'w-3/5',
+    ],
+    metaDataItem: [
+        'flex',
+        'flex-row',
+        'items-center',
+        'gap-1',
+    ],
+    content: [
+        'border-t-2',
+        'border-t-stone-500',
+        'py-20',
+    ]
+
+});
 
 // @ts-ignore
 export default function Blog({ frontmatter, content}) {
