fa1403fff60f45fa36a056141799a04d87476e53
Bedrock02 <jimsteve91@gmail.com>
2023-01-24 11:34:25 -0500

    GA Analytics

--- components/Card/card.tsx
+++ components/Card/card.tsx
@@ -3,6 +3,7 @@ import cardStyles from "./card.styles";
 import Tags from "../Tags/tags";
 import { AiFillRead } from "react-icons/ai";
 import { Post } from '../../types';
+import { event } from '../../lib/gtag';
 
 interface CardProps {
   post: Post
@@ -17,7 +18,11 @@ const Card = ({ post }: CardProps) => {
       </h1>
       <p className={cardStyles.date}>{date}</p>
       <p className={cardStyles.metaData}>{metaDesc}</p>
-      <Link className="text-sky-600" href={`blog/${slug}`}>
+      <Link className="text-sky-600" href={`blog/${slug}`} onClick={() => event({
+          action: 'Read more',
+          category: 'Post CTA',
+          label: title,
+        })}>
         <div className={cardStyles.readMore}>
           <AiFillRead />
           <span>Read More</span>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,30 @@
+/* eslint-disable @next/next/inline-script-id */
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout/layout';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
+import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-      <Analytics />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} /><Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${GA_TRACKING_ID});
+            `
+          }} />
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </>
   );
 }
