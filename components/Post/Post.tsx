cb2c7ef7d12283f662dc9b8f4390dfcf2b416911
Bedrock02 <jimsteve91@gmail.com>
2023-01-31 00:49:18 -0500

    Syntax Highlighting

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -2,6 +2,9 @@ import ReactMarkdown from 'react-markdown';
 import { readTime } from 'lib/utils';
 import postStyles from './post.styles';
 import { AiFillClockCircle, AiFillCalendar } from 'react-icons/ai';
+import rehypeRaw from 'rehype-raw';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 interface Props {
     title: string,
@@ -10,29 +13,75 @@ interface Props {
     content: string,
 }
 
-const Post = ({ title, metaDesc, date, content }: Props) => {
-    return (
-      <div className="w-full">
-        <div className={postStyles.articleContainer}>
-          <section className={postStyles.header}>
-            <h1 className="text-lg-2xl">{title}</h1>
-            <p className={postStyles.metaDesc}>{metaDesc}</p>
-            <div className={postStyles.metaData}>
-              <div className={postStyles.metaDataItem}>
-                <AiFillCalendar />
-                <span>{date}</span>
-              </div>
-              <div className={postStyles.metaDataItem}>
-                <AiFillClockCircle />
-                <span>{`~${readTime(content)} min read`}</span>
-              </div>
-            </div>
-          </section>
-          <article className="all-initial">
-            <ReactMarkdown>{content}</ReactMarkdown>
-          </article>
-        </div>
+/**
+ * Took code from the following blog
+ * https://www.thefullstackblog.com/highlight-code-blocks-in-markdown-files-with-react-markdown-and-react-syntax-highlighter-libraries
+ */
+const CodeBlock = {
+  // @ts-ignore
+  code({ node, inline, className, children, ...props }) {
+    const match = /language-(\w+)/.exec(className || '');
+    return !inline && match ? (
+      <SyntaxHighlighter
+        style={vscDarkPlus}
+        language={match[1]}
+        PreTag="div"
+        {...props}
+      >
+        {String(children).replace(/\n$/, '')}
+      </SyntaxHighlighter>
+    ) : (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
+  },
+};
+
+const PostHeader = ({title, metaDesc, date, content }: Props) =>
+  (
+    <section className={postStyles.header}>
+      <h1 className="text-lg-2xl">{title}</h1>
+      <p className={postStyles.metaDesc}>{metaDesc}</p>
+      <div className={postStyles.metaData}>
+      <div className={postStyles.metaDataItem}>
+          <AiFillCalendar />
+          <span>{date}</span>
       </div>
+      <div className={postStyles.metaDataItem}>
+          <AiFillClockCircle />
+          <span>{`~${readTime(content)} min read`}</span>
+      </div>
+      </div>
+    </section>
+  );
+
+interface PostContentProps {
+  content: string
+}
+
+const PostContent = ({ content }: PostContentProps) => (
+  <article className="all-initial">
+    <ReactMarkdown
+      rehypePlugins={[rehypeRaw]}
+      //@ts-ignore
+      components={CodeBlock}
+    >
+      {content}
+    </ReactMarkdown>
+  </article>
+);
+
+
+const Post = (props: Props) => {
+  const { content } = props;
+  return (
+    <div className="w-full">
+      <div className={postStyles.articleContainer}>
+        <PostHeader {...props} />
+        <PostContent content={content} />
+      </div>
+    </div>
     );
 };
 
