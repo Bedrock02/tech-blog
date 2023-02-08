7112ac7f4b1ea0685c0e59c8c59119dbee92e794
Bedrock02 <jimsteve91@gmail.com>
2023-02-08 13:28:11 -0500

    Add padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home(props: HomeProps) {
       <Head>
         <link rel="payload" href="/images/pexels-stephan-seeber-1054218.jpg" as="image"></link>
       </Head>
-      <div className="flex flex-row flex-wrap gap-20 pt-20 w-4/5 mx-auto">
+      <div className="flex flex-row flex-wrap gap-20 py-20 w-4/5 mx-auto">
         {posts.map((post,index)=>(
           <Card key={index} post={post} />
         ))}
